3561718001383f189ac6d9ac0ba96ed62a9fe953
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-08-05 13:21:29 +0500

    added header

---
+++ client/src/Header.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+export default function Post() {
+    return (
+        <header>
+            <a href="" className="logo">My Blog</a>
+            <nav>
+                <Link to="/login">Login</Link>
+                <Link to="">Register</Link>
+            </nav>
+        </header>
+    );
+}
