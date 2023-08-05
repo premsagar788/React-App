36a2726acd082f18490ce570aef4acc69d2264c9
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-08-05 13:21:36 +0500

    added single post

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://techcrunch.com/wp-content/uploads/2023/07/window-snyder-1.jpg?w=1390&crop=1" />
+            </div>
+            <div className="texts">
+                <h2>Meet Window Snyder, the trailblazer who helped secure the internet and billions of devices</h2>
+                <p className="info">
+                    <a className="author">Prem</a>
+                    <time>2023-01-06 16:45</time>
+                </p>
+                <p className="summary">After the band played Miles Davis’ “Seven Steps to Heaven,” and an effusive introduction from the head of the school, Window Snyder stands in front of a hall filled with around 800 students at her old high school to receive an alumni award.</p>
+            </div>
+        </div>  
+    );
+}
