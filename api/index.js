d863256c290d9260ba6e39b774731a9790cd5614
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-08-08 08:18:37 +0500

    code for signup

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
 
-app.post('/register', (req, res) => {
-    res.json('test ok2');
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://premsaagar:s1SVgGol56zQZU3z@cluster0.fmbc2qk.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.create({username, password});
+    res.json({ requestData: { username, password } });
 });
 
 app.listen(4000);
+
+
+// 
