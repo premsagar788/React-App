cfe37ab4374cb924458fdd370e0e1a66f54f5ecd
Prem Sagar <50700219+premsagar788@users.noreply.github.com>
2023-08-08 11:59:37 +0500

    small fix

--- api/models/User.js
+++ api/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema,model } = mongoose;
 
 const UserSchema = new Schema({
-    username: { type: String, required: true, min: 4, unique: true },
+    username: { type: String, required: true, min: 4 },
     password: { type: String, required: true},
 });
 
